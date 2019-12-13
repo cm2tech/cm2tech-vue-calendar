ebf2c0ae586022836f464124b5b3b58e99f32035
Alan <alan@cm2tech.com.br>
2019-12-13 20:00:50 -0300

    Update package.json

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: path.resolve(__dirname + './../src/index.js'),
   output: {
     path: path.resolve(__dirname + './../dist'),
-    filename: 'v-breadcrumps.js',
+    filename: 'cm2tech-vue-calendar.js',
   },
   module: {
     rules: [
